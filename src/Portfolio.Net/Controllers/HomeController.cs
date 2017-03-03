d949b07fed3b34036691005c972e27879cde9d99
Brittany Rausch <berausch247@gmail.com>
2017-03-03 12:14:32 -0800

    Create the framework and folders for a basic .Net MVC app

---
+++ src/Portfolio.Net/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Portfolio.Net.Models;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Portfolio.Net.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Projects()
+        {
+            var allStarredRepos = Repo.GetStarredRepos();
+            return View(allStarredRepos);
+        }
+    }
+}
---
+++ src/Portfolio.Net/Models/Repo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using RestSharp;
+using RestSharp.Authenticators;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Portfolio.Net.Models
+{
+    public class Repo
+    {
+    }
+}
